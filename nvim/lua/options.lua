9fd5002a6ed42c5a5fedc4f946c6975e4d496bc8
Shi Tian <shitian007@hotmail.com>
2026-07-03 15:46:05 +0800

    Enable relative line numbers in Neovim

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,6 +1,8 @@
 require "nvchad.options"
 
 local o = vim.o
+o.number = true
+o.relativenumber = true
 o.foldmethod = 'expr'
 o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 o.foldlevelstart = 99 -- Default to no folding
