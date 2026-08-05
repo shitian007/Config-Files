c1de4f9490df74443f5b541eaaeaed4384b5675a
Shi Tian <shitian007@hotmail.com>
2026-08-05 10:09:37 +0800

    Disable automatic comment continuation in Neovim

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,1 +1,15 @@
 require "nvchad.autocmds"
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+local disable_auto_comment = augroup("disable_auto_comment", { clear = true })
+
+autocmd("FileType", {
+  group = disable_auto_comment,
+  pattern = "*",
+  desc = "Disable automatic comment continuation",
+  callback = function()
+    vim.opt_local.formatoptions:remove { "c", "r", "o" }
+  end,
+})
