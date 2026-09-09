9b8df5e1b798554243fbaadb993599557b2b15f7
Shi Tian <shitian007@hotmail.com>
2026-09-09 10:32:18 +0800

    Enable Neovim whitespace markers

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,6 +3,8 @@ require "nvchad.options"
 local o = vim.o
 o.number = true
 o.relativenumber = true
+o.list = true
+o.listchars = "tab:»·,trail:·,nbsp:␣"
 o.foldmethod = 'expr'
 o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 o.foldlevelstart = 99 -- Default to no folding
